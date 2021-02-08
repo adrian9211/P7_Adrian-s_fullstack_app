fcdb7bbf7142519674212dfd4fc89e846616e075
adrian9211 <adrian9211@gmail.com>
2021-02-08 13:49:15 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# P7_Adrian-s_fullstack_app
+Full stack application to complete OpenClassrooms last project. Frontend design Vue js, Backend Node.js, Experss and MySQL database. 
+
+To run Frontend server:
+
+## Project setup
+```
+npm install
+```
+##To run Frontend server:
+```
+cd frontend/p7-adrian-nykiel-vue-app
+```
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ frontend/p7_adrian_vue_app/src/views/Home.vue
@@ -0,0 +1,64 @@
+<template>
+<b-container>
+  <b-row allign-v="center">
+    <b-col md="3">
+      <b-card
+        title="Sign In section"
+        img-src= "../assets/icon.png"
+        img-alt="logo"
+        img-top
+        style="max-width:20rem"
+        class="mb-2">
+        <b-card-text>
+          dipisicing elit. Vel facere aliquid error dicta, eaque quidem illo, porro dolores debitis atque odit explicabo earum rerum rem et dolore, quisquam ducimus voluptatem.
+        </b-card-text>
+        <b-button variant="primary">Sign In</b-button>
+      </b-card>
+        <img alt="Vue logo" src= "../assets/icon.png"/>
+    </b-col>
+    
+    <b-col md="3">
+      <b-card
+        title="Sign In section"
+        img-src= "../assets/icon.png"
+        img-alt="logo"
+        img-top
+        style="max-width:20rem"
+        class="mb-2">
+        <b-card-text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel facere aliquid error dicta, eaque quidem illo, porro dolores debitis atque odit explicabo earum rerum rem et dolore, quisquam ducimus voluptatem.
+        </b-card-text>
+        <b-button variant="primary">Sign In</b-button>
+      </b-card>
+    </b-col>
+
+    <b-col md="3">
+      <b-card
+        title="Sign In section"
+        img-src= "../assets/icon.png"
+        img-alt="logo"
+        img-top
+        style="max-width:20rem"
+        class="mb-2">
+        <b-card-text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel facere aliquid error dicta, eaque quidem illo, porro dolores debitis atque odit explicabo earum rerum rem et dolore, quisquam ducimus voluptatem.
+        </b-card-text>
+        <b-button variant="primary">Sign In</b-button>
+      </b-card>
+    </b-col>
+  </b-row>
+</b-container>
+  
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    
+  }
+};
+</script>
