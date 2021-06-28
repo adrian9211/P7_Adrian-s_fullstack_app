e23bdc8bf34e44733793e6c2445b31d0cf273df0
adrian9211 <adrian9211@gmail.com>
2021-06-28 21:13:54 +0100

    .history catalog removal

--- .history/backend/start_20210302175317.js
+++
@@ -1,8 +0,0 @@
-// Transpile all code following this line with babel and use '@babel/preset-env' (aka ES6) preset.
-require("@babel/register")({
-    presets: ["@babel/preset-env"]
-  });
-  
-  import 'babel/register from 'web3';
-  // Import the rest of our application.
-  module.exports = require('./index.js')
